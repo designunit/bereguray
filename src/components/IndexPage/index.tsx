ec391580850f234879155a6edb1ddae4f9b9dc81
manhir <53478716+manhir@users.noreply.github.com>
2020-04-08 17:33:22 +0300

    mobile about

--- src/components/IndexPage/index.tsx
+++ src/components/IndexPage/index.tsx
@@ -4,6 +4,7 @@ import { Title } from '../Title'
 import { PageSection } from '../PageSection'
 import { Header } from '../Header'
 import { Steps } from '../Steps'
+import { About } from '../About'
 
 const IndexPage = () => (
     <main className={styles.layout}>
@@ -71,60 +72,9 @@ const IndexPage = () => (
             </div>
         </PageSection>
 
-        {/* <PageSection>
-            <div 
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignContent: 'center'
-                }}
-            >
-                <img src='/static/unit.jpg' 
-                    style={{
-                        minWidth: '480px',
-                        maxWidth: '33%', 
-                        objectFit: 'contain',
-
-                        marginRight: '128px'
-                    }}
-                />
-                <div style={{maxWidth: '500px'}}>
-                    <h2>
-                        Что такое #берегурай?
-                    </h2>
-                    <p className={styles.description}>
-                        #берегурай - общественная кампания для жителей города Урай
-                    </p>
-                    <p className={styles.description}>
-                        Здесь вы сможете поделиться своим мнением и вместе определить проблемы, потенциалы и ценности для формирования нового общественного городского центра на набережной реки Конды в городе Урай.
-                    </p>
-                    <p className={styles.description}>
-                        Собранные предложения лягут в основу разработки проекта набережной на Всероссийский конкурс Минстрой РФ в 2020 году.
-                    </p>
-                </div>
-            </div> 
-        </PageSection> */}
+        <About />
         
         <Steps />
-
-        {/* <PageSection>
-                    <Button
-                        theme={'primary'}
-                        size={'big'}
-                        style={{
-                            marginRight: '24px',
-                        }}
-                    >
-                        Оставить историю
-                    </Button>
-                    <Button
-                        theme={'default'}
-                        size={'big'}
-                    >
-                        Присоединиться к проекту
-                    </Button>
-
-                </PageSection> */}
     </main>
 )
 
