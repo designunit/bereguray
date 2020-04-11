3ccd2aae2f88b0e38c786b206b3e8fda2d59f29b
Roman Timashev <roman@tmshv.ru>
2020-04-11 05:21:56 +0300

    add react-hook-form

--- src/components/OpinionForm/index.tsx
+++ src/components/OpinionForm/index.tsx
@@ -1,11 +1,40 @@
+import { useForm, OnSubmit } from 'react-hook-form'
+import { useCallback } from 'react'
+
+type FormData = {
+    username: string
+    email: string
+}
+
 export const OpinionForm: React.FC = props => {
+    const { handleSubmit, register, errors } = useForm<FormData>();
+    const onSubmit = useCallback<OnSubmit<FormData>>(values => {
+        console.log(values);
+    }, [])
+
     return (
-        <form>
-            <input />
-            <button>tab navigation</button>
-            <button>stays</button>
-            <button>inside</button>
-            <button>the modal</button>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+                name="email"
+                ref={register({
+                    required: 'Required',
+                    pattern: {
+                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                        message: "invalid email address"
+                    }
+                })}
+            />
+            {errors.email && errors.email.message}
+
+            <input
+                name="username"
+                ref={register({
+                    validate: value => value !== "admin" || "Nice try!"
+                })}
+            />
+            {errors.username && errors.username.message}
+
+            <button type="submit">Submit</button>
         </form>
     )
 }
