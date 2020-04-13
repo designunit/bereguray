a2cdc5e28173b9460ac3d80db19af20c8dca6cc5
manhir <53478716+manhir@users.noreply.github.com>
2020-04-13 14:41:16 +0300

    requested story component

--- src/components/Story/index.tsx
+++ src/components/Story/index.tsx
@@ -9,35 +9,19 @@ export type StoryProps = {
 }
 
 export const Story: React.SFC<StoryProps> = props => (
-    <div className={s.container}>
-        {props.pictureSide === 'left' ? (
-            <>
-                <div className={`${s.picture} ${s.left}`}>
-                    <img src={props.picturePath} style={{ width: '100%' }} />
-                </div>
+    <div className={`${s.container} ${props.pictureSide === 'right' ? s.pictureRight : null}`}>
+        <div className={s.picture}>
+            <img src={props.picturePath} style={{ width: '100%' }} />
+        </div>
 
-                <div className={s.text} >
-                    <div className={s.header}>
-                        <span className={s.title}> {props.title} </span>
-                        <span className={s.subtitle}> {props.subtitle} </span>
-                    </div>
-                    {props.text}
-                </div>
-            </>
-        ) : (
-            <>
-                <div className={`${s.text} ${s.left}`} >
-                    <div className={s.header}>
-                        <span className={s.title}> {props.title} </span>
-                        <span className={s.subtitle}> {props.subtitle} </span>
-                    </div>
-                    {props.text}
-                </div>
+        <div className={s.spacer} />
 
-                <div className={s.picture}>
-                    <img src={props.picturePath} style={{ width: '100%' }} />
-                </div>
-            </>
-        )}
+        <div>
+            <div className={s.header}>
+                <span className={s.title}> {props.title} </span>
+                <span className={s.subtitle}> {props.subtitle} </span>
+            </div>
+            {props.text}
+        </div>
     </div>
 )
