3541161e150d6eea328c779e8a1fc7fa62afebfa
Roman Timashev <roman@tmshv.ru>
2020-04-13 19:26:50 +0300

    latlng extension keep on select only

--- latlng/extension.js
+++ latlng/extension.js
@@ -1,3 +1,39 @@
 importScripts('/latlng.js')
 
-console.log('BEREGURAY APP')
+const typeLabel = new Map([
+	['idea', 'Идея'],
+	['problem', 'Пиздец'],
+	['nice', 'Милота'],
+])
+
+setup(async () => ({
+    name: 'Bereguray',
+    version: '1.0.0',
+    description: 'https://берегурай.рф'
+}))
+
+on('install', async event => {
+	
+})
+
+on('feature.select', async event => {
+	const featureId = event.data.featureId
+	const layerId = event.data.layerId
+	if(!featureId){
+		return
+	}
+	
+    const fc = await requestFeatures([featureId])
+    
+    const feature = fc.features[0]
+    const geometryType = feature.geometry.type
+    assert(geometryType !== 'Point', new Error('Selected feature is not a point'))
+    
+	const type = feature.properties['type']
+    const data = [
+    	{ key: 'Это', value: typeLabel.get(type) },
+    	{ key: 'Чо сказал', value: feature.properties['comment']},
+    ]
+
+	await showMapPopup(feature.geometry.coordinates, ['kv', { data }])
+})
