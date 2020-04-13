af6c037914f6e0fa179135d364556ab2e7a60a70
manhir <53478716+manhir@users.noreply.github.com>
2020-04-13 15:02:32 +0300

    stories as component

--- src/components/IndexPage/index.tsx
+++ src/components/IndexPage/index.tsx
@@ -3,10 +3,9 @@ import { Button } from '../Button'
 import { Title } from '../Title'
 import { PageSection } from '../PageSection'
 import { Header } from '../Header'
-import { Story } from '../Story'
-import { JoinForm } from '../JoinForm'
 import { Steps } from '../Steps'
 import { About } from '../About'
+import { Stories } from '../Stories'
 
 const IndexPage = () => (
     <main className={styles.layout}>
@@ -72,81 +71,11 @@ const IndexPage = () => (
             </div>
         </PageSection>
 
-        <div className={styles.stories}>
-            <span style={{ marginBottom: '48px' }}>
-                <Title>
-                    Жители о набережной
-                </Title>
-            </span>
-            <Story
-                pictureSide='left'
-                picturePath='/static/uray1.jpg'
-                title={(
-                    <h2>
-                        Алена Апина
-                    </h2>
-                )}
-                subtitle={(
-                    <h3>Кто такая по жизни</h3>
-                )}
-                text={(
-                    <p className={styles.description}>
-                        Мой батя ебашит вообще адовые блюда.<br/>
-                        Ну такой вот примерно рецепт усредненный, потому что вариаций масса. 
-                        Берется суп, он не греется, греть - это не про моего батю. Он берет это суп, вываливает его на сковороду и начинает жарить. Добавляет в него огромное количество лука, чеснока, перца черного и красного МУКИ! для вязкости, томатная паста сверху.
-                    </p>
-                )}
-            />
-            <Story
-                pictureSide='right'
-                picturePath='/static/uray2.jpg'
-                title={(
-                    <h2>
-                        Алена Апина
-                    </h2>
-                )}
-                subtitle={(
-                    <h3>subtitle</h3>
-                )}
-                text={(
-                    <p className={styles.description}>
-                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.<br/><br/> Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus 
-                        mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-                    </p>
-                )}
-            />
-
-            <div style={{
-                width: '100%',
-                display: 'flex',
-                flexWrap: 'wrap',
-                justifyContent: 'center',
-
-            }}>
-                <span className={styles.storiesButton}>
-                    <Button
-                        size='big'
-                        theme='primary'
-                        disabled
-                    >
-                        Оставить историю
-                    </Button>
-                </span>
-                <Button
-                    size='big'
-                    disabled
-                >
-                    Присоедениться к проекту
-                </Button>
-            </div>
-        </div>
-
-        <div className={styles.form}>
+        {/* <div className={styles.form}>
             <Title>
                 Присоединяйся к движению
             </Title>
-            <JoinForm />
-        </div>
+        </div> */}
     </main>
 )
 
