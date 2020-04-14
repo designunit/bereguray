881b69cd05c38d8abb65870f2e373307ff9b89e4
manhir <53478716+manhir@users.noreply.github.com>
2020-04-14 19:54:26 +0300

    parallax for index

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,13 @@ import 'src/style.css'
 import Head from 'next/head'
 import { AppType } from 'next/dist/next-server/lib/utils'
 import { YMetrika } from 'src/components/YMetrika'
-import { ParallaxProvider } from 'react-scroll-parallax';
 
 const App: AppType = props => {
     const { Component, pageProps } = props
     const metrika = process.env.YANDEX_METRIKA
 
     return (
-        <ParallaxProvider>
+        <>
             <Head>
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, maximum-scale=1.0" />
@@ -26,7 +25,7 @@ const App: AppType = props => {
             </Head>
 
             <Component {...pageProps} />
-        </ParallaxProvider>
+        </>
     )
 }
 
