efd6f22d4bc992aeb399bf70f710b02535a591f9
Roman Timashev <roman@tmshv.ru>
2020-04-28 21:21:53 +0300

    project page to slides

--- src/components/PageLayout/index.tsx
+++ src/components/PageLayout/index.tsx
@@ -1,26 +1,14 @@
-import styles from './styles.module.css'
 import { Header } from 'src/components/Header'
 import { Button } from 'src/components/Button'
 
-const ProjectPage = () => {
-
-    const slides = []
-    const slidesCount = 8
-
-    for (let i = 0; i < slidesCount; i++) {
-        slides[i] = (
-            <img
-                className={styles.picture} 
-                src={`/static/project/YRAI_${i+1}.png`}
-            />
-        )
-    }
-
+export const PageLayout: React.SFC = props => {
     return (
         <main>
             <Header
                 menu={(
                     <>
+                        <Button href={'/'} theme={'link'}>#</Button>
+                        <Button href={'/project'} theme={'link'}>Проект</Button>
                         <Button href={'https://app.latl.ng/map/bereguray'} theme={'link'}>Карта идей</Button>
                         <Button disabled theme={'link'}>Вопрос/Ответ</Button>
                         <Button disabled theme={'link'}>Команда</Button>
@@ -36,11 +24,7 @@ const ProjectPage = () => {
                 )}
             />
 
-            <div className={styles.slidesContainer}>
-                {slides}
-            </div>
+            {props.children}
         </main>
     )
 }
-
-export default ProjectPage
