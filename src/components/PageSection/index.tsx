1a7f0b746138d35d1827d0902281f27f85b05b58
manhir <53478716+manhir@users.noreply.github.com>
2020-04-29 19:48:35 +0300

    just disable parallax on mobile for now

--- src/components/PageSection/index.tsx
+++ src/components/PageSection/index.tsx
@@ -6,42 +6,24 @@ export type PageSectionProps = {
     back?: React.ReactNode
 }
 
-const ParallaxHOC: React.SFC = props => {
-    const isMobile = useMedia('(max-width: 480px)')
-
-    return (
-        isMobile ? (
-            <div style={{
-                width: '100%',
-                height: '100%',
-                position: 'fixed',
-            }}>
-                {props.children}
-            </div>
-        ) : (
-            <Parallax
-                y={[-50, 50]}
-                styleOuter={{
-                    height: '100%',
-                }}
-                styleInner={{
-                    height: '100%',
-                }}
-            >
-                {props.children}
-            </Parallax>
-        )
-    )
-}
-
 export const PageSection: React.SFC<PageSectionProps> = props => {
-    
+    const isMobile = useMedia('(max-width: 480px)')
+        
     return (
         <section className={s.section}>
             <div className={s.back}>
-                <ParallaxHOC>
+                <Parallax
+                    y={[-50, 50]}
+                    styleOuter={{
+                        height: '100%',
+                    }}
+                    styleInner={{
+                        height: '100%',
+                    }}
+                    disabled={isMobile}
+                >
                     {props.back}
-                </ParallaxHOC>
+                </Parallax>
             </div>
             <div className={s.content}>
                 {props.children}
