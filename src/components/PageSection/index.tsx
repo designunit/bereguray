4eb5334bc2c9cead34a1cf0de5403f4f43b2264c
manhir <53478716+manhir@users.noreply.github.com>
2020-04-29 17:40:53 +0300

    fallback for mobile parallax

--- src/components/PageSection/index.tsx
+++ src/components/PageSection/index.tsx
@@ -1,16 +1,51 @@
 import s from './styles.module.css'
+import { useMedia } from 'react-use';
+import { Parallax } from 'react-scroll-parallax';
 
 export type PageSectionProps = {
     back?: React.ReactNode
 }
 
-export const PageSection: React.SFC<PageSectionProps> = props => (
-    <section className={s.section}>
-        <div className={s.back}>
-            {props.back}
-        </div>
-        <div className={s.content}>
-            {props.children}
-        </div>
-    </section>
-)
+const ParallaxHOC: React.SFC = props => {
+    const isMobile = useMedia('(max-width: 480px)')
+
+    return (
+        isMobile ? (
+            <div style={{
+                width: '100%',
+                height: '100%',
+                position: 'fixed',
+            }}>
+                {props.children}
+            </div>
+        ) : (
+            <Parallax
+                y={[-50, 50]}
+                styleOuter={{
+                    height: '100%',
+                }}
+                styleInner={{
+                    height: '100%',
+                }}
+            >
+                {props.children}
+            </Parallax>
+        )
+    )
+}
+
+export const PageSection: React.SFC<PageSectionProps> = props => {
+    
+    return (
+        <section className={s.section}>
+            <div className={s.back}>
+                <ParallaxHOC>
+                    {props.back}
+                </ParallaxHOC>
+            </div>
+            <div className={s.content}>
+                {props.children}
+            </div>
+        </section>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { NextPage } from 'next'
 import { Hero } from 'src/components/Hero'
-import { ParallaxProvider, Parallax } from 'react-scroll-parallax'
+import { ParallaxProvider } from 'react-scroll-parallax'
 import { PageSection } from 'src/components/PageSection'
 import { Stories } from 'src/components/Stories'
 import { Bubble } from 'src/components/Bubble'
@@ -14,42 +14,31 @@ const Index: NextPage = () => (
 
         <PageSection
             back={(
-                <Parallax
-                    y={[-50, 50]}
-                    styleOuter={{
-                        height: '100%',
-                    }}
-                    styleInner={{
-                        height: '100%',
-                    }}
-                >
-                    <div style={{
-                        width: '100%',
-                        height: '100%',
-                        backgroundImage: 'url("/static/uray2.jpg")',
-                        backgroundSize: 'auto 100%',
-                        backgroundPosition: '100% top',
-                        backgroundRepeat: 'no-repeat',
-                    }}>
-                        <Bubble
-                            style={{
-                                position: 'relative',
-                                left: '50%',
-                                height: '100%',
-                                transform: `scale(1.75) rotate(${Math.random() * 360}deg)`,
-                            }}
-                            opacity={.5}
-                            // color='#E7ECFF'
-                            color='#25aae1'
-                        />
-                    </div>
-                </Parallax>
+                <div style={{
+                    width: '100%',
+                    height: '100%',
+                    backgroundImage: 'url("/static/uray2.jpg")',
+                    backgroundSize: 'auto 100%',
+                    backgroundPosition: '100% top',
+                    backgroundRepeat: 'no-repeat',
+                }}>
+                    <Bubble
+                        style={{
+                            position: 'relative',
+                            left: '50%',
+                            height: '100%',
+                            transform: `scale(2)`,
+                        }}
+                        opacity={.5}
+                        color='#25aae1'
+                    />
+                </div>
             )}
         >
             <Hero />
         </PageSection>
 
-        <Stories></Stories>
+        <Stories />
     </ParallaxProvider>
 )
 
