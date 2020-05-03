a7da83e73797d8f0c8da245c0a12e00e2e2b89e3
Roman Timashev <roman@tmshv.ru>
2020-05-04 00:16:25 +0300

    add faq

--- src/components/Story/index.tsx
+++ src/components/Story/index.tsx
@@ -2,39 +2,43 @@ import s from './styles.module.css'
 import { Bubble } from '../Bubble'
 
 export type StoryProps = {
-    picturePath: string
+    picturePath?: string
     title: React.ReactNode
-    subtitle: React.ReactNode
-    text: React.ReactNode
+    subtitle?: React.ReactNode
+    text?: React.ReactNode
     pictureSide: 'left' | 'right'
 }
 
 export const Story: React.SFC<StoryProps> = props => (
     <div className={`${s.container} ${props.pictureSide === 'right' ? s.pictureRight : null}`}>
         <div className={s.picture}>
-            <Bubble 
-                style={{
-                    position: 'absolute',
-                    maxWidth: '100%',
-                    maxHeight: '100%',
-                    transform: `scale(1.1)`,
-                }}
-                color='#B1F4EC'
-            />
-            <Bubble 
-                style={{
-                    position: 'absolute',
-                    maxWidth: '100%',
-                    maxHeight: '100%',
-                }}
-                color='#FFD166'
-            />
-            <Bubble 
-                style={{
-                    width: '100%',
-                }}
-                picturePath={props.picturePath}
-            />
+            {!props.picturePath ? null : (
+                <>
+                    <Bubble
+                        style={{
+                            position: 'absolute',
+                            maxWidth: '100%',
+                            maxHeight: '100%',
+                            transform: `scale(1.1)`,
+                        }}
+                        color='#B1F4EC'
+                    />
+                    <Bubble
+                        style={{
+                            position: 'absolute',
+                            maxWidth: '100%',
+                            maxHeight: '100%',
+                        }}
+                        color='#FFD166'
+                    />
+                    <Bubble
+                        style={{
+                            width: '100%',
+                        }}
+                        picturePath={props.picturePath}
+                    />
+                </>
+            )}
         </div>
 
         <div className={s.spacer} />
@@ -42,9 +46,12 @@ export const Story: React.SFC<StoryProps> = props => (
         <div>
             <div className={s.header}>
                 <span className={s.title}> {props.title} </span>
-                <span className={s.subtitle}> {props.subtitle} </span>
+                {!props.subtitle ? null : (
+                    <span className={s.subtitle}>{props.subtitle}</span>
+                )}
             </div>
             {props.text}
+            {props.children}
         </div>
     </div>
 )
