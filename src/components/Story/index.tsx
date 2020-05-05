a052766251cfab650dbc432d1752ad54fd8c2edf
Sergey <stretched.8@gmail.com>
2020-05-05 06:04:40 +0300

    avatar

--- src/components/Story/index.tsx
+++ src/components/Story/index.tsx
@@ -1,6 +1,25 @@
 import s from './styles.module.css'
 import { Bubble } from '../Bubble'
 
+const Ratio: React.SFC<{ ratio: number, className?: string }> = props => (
+    <div className={props.className} style={{
+        width: '100%',
+        height: 0,
+        position: 'relative',
+        paddingBottom: `${props.ratio * 100}%`,
+    }}>
+        <div style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+        }}>
+            {props.children}
+        </div>
+    </div>
+)
+
 export type StoryProps = {
     picturePath?: string
     title?: React.ReactNode
@@ -11,42 +30,39 @@ export type StoryProps = {
 
 export const Story: React.SFC<StoryProps> = props => (
     <div className={`${s.container} ${props.pictureSide === 'right' ? s.pictureRight : null}`}>
-        <div className={s.picture}>
-            {!props.picturePath ? null : (
-                <>
+        {!props.picturePath ? null : (
+            <div className={s.avatar}>
+                <Ratio ratio={1}>
                     <Bubble
                         style={{
                             position: 'absolute',
-                            maxWidth: '100%',
-                            maxHeight: '100%',
-                            height: '100%',
+                            maxWidth: '200%',
+                            maxHeight: '200%',
+                            height: '200%',
                             width: 'fit-content',
+                            zIndex: -4,
                         }}
                         color='#B1F4EC'
                     />
                     <Bubble
                         style={{
                             position: 'absolute',
-                            maxWidth: '100%',
-                            maxHeight: '100%',
-                            height: '100%',
+                            maxWidth: '120%',
+                            maxHeight: '120%',
+                            height: '120%',
                             width: 'fit-content',
+                            zIndex: -3,
                         }}
                         color='#FFD166'
                     />
-                    <Bubble
-                        style={{
-                            width: '100%',
-                        }}
-                        picturePath={props.picturePath}
+                    <img
+                        src={props.picturePath}
                     />
-                </>
-            )}
-        </div>
+                </Ratio>
+            </div>
+        )}
 
-        {!props.picturePath ? null : <div className={s.spacer} />}
-
-        <div>
+        <div className={s.text}>
             <div className={s.header}>
                 <span className={s.title}> {props.title} </span>
                 {!props.subtitle ? null : (
